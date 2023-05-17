65ec09ce3f88d3c170a1e9ce7ffd1ef39da0f317
kaybrian <kayonbrian@gmail.com>
2023-05-17 22:35:59 +0200

    make a new way to say the info

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,6 +15,7 @@ class FileStorage:
         __objects: dictionary
     """
     __file_path = "file.json"
+    # Stores all objects by <class name>.id
     __objects = {}
 
     def all(self):
@@ -29,18 +30,18 @@ class FileStorage:
         """
         if obj:
             """Check if the object exits then save it"""
-            key = obj.__class__.__name__ + "." + obj.id
+            key = obj.__class__.__name__ + "." + str(obj.id)
             FileStorage.__objects[key] = obj
 
     def save(self):
         """
         serializes __objects to the JSON file (path: __file_path)
         """
-        new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(new_dict, f)
+        object_dict = {}
+        for obj in self.__objects:
+            object_dict[obj] = self.__objects[obj].to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(object_dict, file)
 
     def reload(self):
         """
