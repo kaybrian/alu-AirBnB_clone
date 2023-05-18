96c8977f25357eee374d5bcb32a9e0cb390b5957
kaybrian <kayonbrian@gmail.com>
2023-05-18 11:05:13 +0200

    sned the file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """Defines FileStorage class."""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
@@ -38,7 +45,14 @@ class FileStorage:
         (__file_path) exists ; otherwise, do nothing. If the file does not
         exist, no exception should be raised)
         """
-        
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.place import Place
+        from models.amenity import Amenity
+        from models.review import Review
+
 
         try:
             with open(self.__file_path) as file:
