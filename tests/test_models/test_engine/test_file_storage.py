ef88d0b8bbb5198029fe1891dd343ce833e706c2
kaybrian <kayonbrian@gmail.com>
2023-05-19 13:52:15 +0200

    base mode file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
-Test the File Storage
+    Defines unittests for 'models/engine/file_storage.py'
+    Unittest classes:
+        TestFileStorage_instantiation
+        TestFileStorage_methods
 """
 import os
 import json
@@ -18,7 +21,9 @@ from models.review import Review
 
 
 class TestFileStorage_instantiation(unittest.TestCase):
-    """Unittests for testing instantiation of the FileStorage class."""
+    """
+        Unittests to test instantiation of the 'FileStorage' class.
+    """
 
     def test_FileStorage_instantiation_no_args(self):
         self.assertEqual(type(FileStorage()), FileStorage)
@@ -38,7 +43,9 @@ class TestFileStorage_instantiation(unittest.TestCase):
 
 
 class TestFileStorage_methods(unittest.TestCase):
-    """Unittests for testing methods of the FileStorage class."""
+    """
+        Unittests to testing methods of the 'FileStorage' class.
+    """
 
     @classmethod
     def setUp(self):
